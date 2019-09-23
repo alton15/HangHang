66484573e7e57cab972e02dbbf9150238504d213
alton15 <alton15@naver.com>
2019-09-23 10:15:41 +0900

    [Update] 9/23 권한 요청 위치 변경 및 블루투스 통신 추가

--- HangHangTwo/app/src/main/java/com/example/hanghangtwo/LoadingActivity.java
+++ HangHangTwo/app/src/main/java/com/example/hanghangtwo/LoadingActivity.java
@@ -1,19 +1,86 @@
 package com.example.hanghangtwo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoadingActivity extends AppCompatActivity {
+
+    private String[] permissions = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+    private static final int MULTIPLE_PERMISSIONS = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermissions();
+        }
+
         startLoading();
+    }
+
+    // 권한 요청
+    public boolean checkPermissions() {
+        int result;
+        List<String> permissionList = new ArrayList<>();
+        for (String pm : permissions) {
+            result = ContextCompat.checkSelfPermission(this, pm);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                permissionList.add(pm);
+            }
+        }
+        if (!permissionList.isEmpty()) {
+            ActivityCompat.requestPermissions(this,
+                    permissionList.toArray(new String[permissionList.size()]),
+                    MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MULTIPLE_PERMISSIONS: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < permissions.length; i++) {
+                        if (permissions[i].equals(this.permissions[i])) {
+                            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                                showToast_PermissionDeny();
+                            }
+                        }
+                    }
+                } else {
+                    showToast_PermissionDeny();
+                }
+                return;
+            }
+        }
+    }
+
+    private void showToast_PermissionDeny() {
+        Toast.makeText(getApplicationContext(),
+                "권한 요청에 동의해야 서비스 이용이 가능합니다. 설정에서 권한을 허용해주십시오.",
+                Toast.LENGTH_SHORT).show();
+        finish();
     }
 
     private void startLoading() {
